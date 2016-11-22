0d3daa08ce1bce461d5ce3e35f637645e545958f
CasaTaloyum <casatwy@msn.com>
2016-11-22 19:55:04 +0800

    modification

--- config.sh
+++ config.sh
@@ -10,7 +10,6 @@ homePage=""
 confirmed="n"
 
 getProjectName() {
-    echo -e "\n"
     read -p "Enter Project Name: " projectName
 
     if test -z "$projectName"; then
@@ -43,16 +42,15 @@ getHomePage() {
 }
 
 getInfomation() {
-	echo -e "\n"
     getProjectName
     getHTTPSRepo
     getSSHRepo
     getHomePage
 
     echo -e "\n${Default}================================================"
-    echo -e "  Project Name  :  ${Cyan}${ProjectName}${Default}"
+    echo -e "  Project Name  :  ${Cyan}${projectName}${Default}"
     echo -e "  HTTPS Repo    :  ${Cyan}${httpsRepo}${Default}"
-    echo -e "  SSH Repo      :  ${Cyan}${httpsRepo}${Default}"
+    echo -e "  SSH Repo      :  ${Cyan}${sshRepo}${Default}"
     echo -e "  Home Page URL :  ${Cyan}${homePage}${Default}"
     echo -e "================================================\n"
 }
@@ -73,25 +71,27 @@ specFilePath="../${projectName}/${projectName}.podspec"
 readmeFilePath="../${projectName}/readme.md"
 uploadFilePath="../${projectName}/upload.sh"
 
-cp ./templates/FILE_LICENSE "$licenseFilePath"
-cp ./templates/gitignore    "$gitignoreFilePath"
-cp ./templates/pod.podspec  "$specFilePath"
-cp ./templates/readme.md    "$readmeFilePath"
-cp ./templates/upload.sh    "$uploadFilePath"
+cp -f ./templates/FILE_LICENSE "$licenseFilePath"
+cp -f ./templates/gitignore    "$gitignoreFilePath"
+cp -f ./templates/pod.podspec  "$specFilePath"
+cp -f ./templates/readme.md    "$readmeFilePath"
+cp -f ./templates/upload.sh    "$uploadFilePath"
 
-sed -i "" "s:__PojectName__:${projectName}:g" "$gitignoreFilePath"
-sed -i "" "s:__PojectName__:${projectName}:g" "$readmeFilePath"
-sed -i "" "s:__PojectName__:${projectName}:g" "$uploadFilePath"
+sed -i "" "s%__ProjectName__%${projectName}%g" "$gitignoreFilePath"
+sed -i "" "s%__ProjectName__%${projectName}%g" "$readmeFilePath"
+sed -i "" "s%__ProjectName__%${projectName}%g" "$uploadFilePath"
 
-sed -i "" "s:__PojectName__:${projectName}:g" "$specFilePath"
-sed -i "" "s:__HomePage__:${homePage}:g"      "$specFilePath"
-sed -i "" "s:__HTTPSRepo__:${httpsRepo}:g"    "$specFilePath"
+sed -i "" "s%__ProjectName__%${projectName}%g" "$specFilePath"
+sed -i "" "s%__HomePage__%${homePage}%g"      "$specFilePath"
+sed -i "" "s%__HTTPSRepo__%${httpsRepo}%g"    "$specFilePath"
 
-cd ./$projectName
-git remote add origin $sshRepo
-git rm -rf --cached $projectName.xcodeproj/xcuserdata/
-git rm -rf --cached $projectName.xcworkspace/
-git rm -rf --cached ./Pods/
-git rm -rf --cached __ProjectName__.xcodeproj/project.xcworkspace/xcuserdata/
-git rm --cached Podfile.lock
-git rm --cached .DS_Store
+cd ../$projectName
+git remote add origin $sshRepo  &> /dev/null
+git rm -rf --cached ./Pods/     &> /dev/null
+git rm --cached Podfile.lock    &> /dev/null
+git rm --cached .DS_Store       &> /dev/null
+git rm -rf --cached $projectName.xcworkspace/           &> /dev/null
+git rm -rf --cached $projectName.xcodeproj/xcuserdata/  &> /dev/null
+git rm -rf --cached $projectName.xcodeproj/project.xcworkspace/xcuserdata/ &> /dev/null
+say "finished"
+echo "finished"
