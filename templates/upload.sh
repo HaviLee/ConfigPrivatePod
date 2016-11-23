2aeed9b649540dcf058c0d06ec3fc95c2d3c59a9
CasaTaloyum <casatwy@msn.com>
2016-11-23 09:26:04 +0800

    modification

--- templates/upload.sh
+++ templates/upload.sh
@@ -1,1 +1,1 @@
-cd ~/.cocoapods/repos/BLRepositories && git pull origin master && cd - && pod repo push BLRepositories __ProjectName__.podspec --verbose --allow-warnings
+pod repo push BLRepositories __ProjectName__.podspec --verbose --allow-warnings
