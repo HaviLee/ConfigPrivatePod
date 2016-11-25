4a2f968905e8e9663707785bf30037a7ae43e786
CasaTaloyum <casatwy@msn.com>
2016-11-25 10:51:40 +0800

    modification

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull origin master
+
 Cyan='\033[0;36m'
 Default='\033[0;m'
 
@@ -72,13 +74,20 @@ readmeFilePath="../${projectName}/readme.md"
 uploadFilePath="../${projectName}/upload.sh"
 podfilePath="../${projectName}/Podfile"
 
+echo "copy to $licenseFilePath"
 cp -f ./templates/FILE_LICENSE "$licenseFilePath"
+echo "copy to $gitignoreFilePath"
 cp -f ./templates/gitignore    "$gitignoreFilePath"
+echo "copy to $specFilePath"
 cp -f ./templates/pod.podspec  "$specFilePath"
+echo "copy to $readmeFilePath"
 cp -f ./templates/readme.md    "$readmeFilePath"
+echo "copy to $uploadFilePath"
 cp -f ./templates/upload.sh    "$uploadFilePath"
+echo "copy to $podfilePath"
 cp -f ./templates/Podfile      "$podfilePath"
 
+echo "editing..."
 sed -i "" "s%__ProjectName__%${projectName}%g" "$gitignoreFilePath"
 sed -i "" "s%__ProjectName__%${projectName}%g" "$readmeFilePath"
 sed -i "" "s%__ProjectName__%${projectName}%g" "$uploadFilePath"
@@ -87,7 +96,9 @@ sed -i "" "s%__ProjectName__%${projectName}%g" "$podfilePath"
 sed -i "" "s%__ProjectName__%${projectName}%g" "$specFilePath"
 sed -i "" "s%__HomePage__%${homePage}%g"      "$specFilePath"
 sed -i "" "s%__HTTPSRepo__%${httpsRepo}%g"    "$specFilePath"
+echo "edit finished"
 
+echo "cleaning..."
 cd ../$projectName
 git init
 git remote add origin $sshRepo  &> /dev/null
@@ -97,5 +108,6 @@ git rm --cached .DS_Store       &> /dev/null
 git rm -rf --cached $projectName.xcworkspace/           &> /dev/null
 git rm -rf --cached $projectName.xcodeproj/xcuserdata/`whoami`.xcuserdatad/xcschemes/$projectName.xcscheme &> /dev/null
 git rm -rf --cached $projectName.xcodeproj/project.xcworkspace/xcuserdata/ &> /dev/null
+echo "clean finished"
 say "finished"
 echo "finished"
